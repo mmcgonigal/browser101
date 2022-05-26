9809bd794af6fb0c020e32834da399a47cfd9a60
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-05-25 22:31:33 -0500

    node

--- js/007.js
+++ js/007.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const vertical=document.querySelector(".vertical");
+const vertical=document.querySelector(".vertical")
 const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target")
 const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove",function(event){
+document.addEventListener("mousemove",(event)=>{
     const x = event.clientX;
     const y = event.clientY;
     console.log(`${x} ${y}`)
