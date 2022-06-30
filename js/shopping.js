1e925c30aff8bdaed8ceb9871740c28534819f11
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-29 23:47:08 -0500

    addBtn

--- js/shopping.js
+++ js/shopping.js
@@ -1,1 +1,18 @@
 'use strict'
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer_input");
+const addBtn =  document.querySelector(".footer_button");
+
+
+function onAdd(){
+    //take input from user
+    const text = input.value;
+    console.log(text);
+    //create new item
+    //add new item in intems container
+    // reset input
+}
+
+addBtn.addEventListener("click",()=>{
+    onAdd();
+})
