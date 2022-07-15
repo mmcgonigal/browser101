e908d3a24687c183c331b8d50bd39ed3a2845dea
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-14 23:41:37 -0500

    function recap

--- js/variable.js
+++ js/variable.js
@@ -32,3 +32,32 @@ console.log(obj3.name);
 obj3.name= "james";
 
 console.log(obj3.name);
+
+const num1 = 1 ;
+const num2 = 2;
+const result = num1 + num2;
+console.log(result);
+
+const num3 = 1;
+const num4 =2 ;
+const result2 = num3 +num4
+console.log(result2);
+
+function add (a,b) {
+    return a+b;
+}
+const sum = add (num3, num4);
+console.log(sum);
+const doSomething = add ;
+const result3 = doSomething(4,5);
+console.log(result3);
+const result4 = add(3,10);
+console.log(result4);
+
+function printSomething(){
+    console.log("print something");
+}
+function printSomething1(a, b){
+    console.log(`print this ${a}, and ${b}`)
+}
+printSomething1(100,1111)
