af7ce066466062e7e4011979f8b4530bc15cc42f
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-14 23:20:07 -0500

    recap js, variables

--- js/variable.js
+++ js/variable.js
@@ -13,3 +13,22 @@ let obj = {
 console.log(obj.name);
 let obj2= obj;
 console.log(obj2.name);
+obj.name="james";
+console.log(obj.name);
+console.log(obj2.name);
+
+let a = 2;
+a = 5 ;
+a = 9  // it is okay
+
+const obj3 = {
+    name : "Mina",
+    age : 35,
+    city : "san antonio",
+}
+
+console.log(obj3.name);
+
+obj3.name= "james";
+
+console.log(obj3.name);
