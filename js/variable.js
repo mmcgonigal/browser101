090d0c978a21fec2e06ccc2c5b5ae27ed4a5cf8b
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-14 23:09:49 -0500

    recap js

---
+++ js/variable.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let number = 2;
+let number2 = number;
+console.log(number);
+console.log(number2);
+
+//object
+let obj = {
+    name:'mina',
+    number: 35,
+};
+console.log(obj.name);
+let obj2= obj;
+console.log(obj2.name);
